caeafbece08d65a6a763f9bcfa2b6266d55cf43b
igalna <igalna@gmail.com>
2015-02-17 17:51:36 +0000

    Experimenting with Audio streaming

---
+++ AudioStreaming/src/server/AudioServerTest.java
@@ -0,0 +1,43 @@
+package server;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.*;
+
+import javax.sound.sampled.*;
+
+public class AudioServerTest {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			DatagramSocket socket = new DatagramSocket(10012);
+			
+			File file = new File("");
+			
+			AudioInputStream ais = AudioSystem.getAudioInputStream(file);
+			
+			AudioFormat af = ais.getFormat();
+			
+			SourceDataLine line;
+			DataLine.Info info = new DataLine.Info(TargetDataLine.class, af);
+			line = (SourceDataLine) AudioSystem.getLine(info);
+			line.open(af);
+			line.start();
+			
+			
+		
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
