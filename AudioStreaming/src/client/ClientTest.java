6f6b853a3e9674b93fd2377e447d885cd51a29cc
igalna <igalna@gmail.com>
2015-02-19 12:07:53 +0000

    Testing out functionality for UDP server/client

--- AudioStreaming/src/client/ClientTest.java
+++ AudioStreaming/src/client/ClientTest.java
@@ -5,6 +5,9 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
 
 
@@ -14,7 +17,25 @@ public class ClientTest {
 		
 		try {
 			
-			Socket client = new Socket("localhost", 10010);
+			Socket client = new Socket("localhost", 10013);
+			
+			DatagramSocket udpClient = new DatagramSocket(0);
+			udpClient.setSoTimeout(1000);
+			
+			InetAddress host = InetAddress.getByName("localhost");
+			DatagramPacket request = new DatagramPacket(new byte[1024], 1024, host, 10014);
+			
+			byte[] data = new byte[1024];
+			DatagramPacket response = new DatagramPacket(data, data.length);
+			
+			udpClient.send(request);
+			udpClient.receive(response);
+			
+			String testString = new String(response.getData(), 0, response.getLength(), "US-ASCII");
+			
+			System.out.println(testString);
+			
+			
 			
 			DataInputStream is = new DataInputStream(client.getInputStream());
 			DataOutputStream os = new DataOutputStream(client.getOutputStream());
@@ -24,6 +45,7 @@ public class ClientTest {
 			System.out.println(br.readLine());
 			
 			client.close();
+			udpClient.close();
 			
 		} catch (IOException e) {
 			e.printStackTrace();
--- AudioStreaming/src/server/ServerTest.java
+++ AudioStreaming/src/server/ServerTest.java
@@ -5,17 +5,32 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+
 import javax.sound.*;
 
 public class ServerTest {
 
 		public static void main(String[] args) {
 			try {
-				ServerSocket server = new ServerSocket(10010);
-				
+				ServerSocket server = new ServerSocket(10013);
 				Socket client = server.accept();
+				
+				DatagramSocket udpServer = new DatagramSocket(10014);
+				DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
+				udpServer.receive(request);
+				
+				String testString = "Hello I am a string";
+				byte[] data = testString.getBytes("US-ASCII");
+				InetAddress host = request.getAddress();
+				int port = request.getPort();
+				DatagramPacket response = new DatagramPacket(data, data.length, host, port);
+				
+				udpServer.send(response);
 				
 				DataInputStream is = new DataInputStream(client.getInputStream());
 				DataOutputStream os = new DataOutputStream(client.getOutputStream());
@@ -24,7 +39,9 @@ public class ServerTest {
 				
 				client.close();
 				
+				os.close();
 				server.close();
+				udpServer.close();
 				
 			} catch (IOException e) {
 				e.printStackTrace();
