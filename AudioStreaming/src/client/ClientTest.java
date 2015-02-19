56019daafce84ab1b6f3d770d39799b415796268
igalna <igalna@gmail.com>
2015-02-19 17:25:15 +0000

    Sockets now close in a finally clause

--- AudioStreaming/src/client/ClientTest.java
+++ AudioStreaming/src/client/ClientTest.java
@@ -12,44 +12,66 @@ import java.net.Socket;
 
 
 public class ClientTest {
+	
+	Socket client;
+	DatagramSocket udpClient;
+	InetAddress host;
+	
+	
+	DataInputStream is;
+	DataOutputStream os;
+	InputStreamReader isr;
+	BufferedReader br;
+	
 
 	public static void main(String[] args) {
 		
+		ClientTest ct = new ClientTest();
+		ct.run();
+	}
+
+	
+	private void run() {
+		
 		try {
+			client = new Socket("localhost", 10027);
 			
-			Socket client = new Socket("localhost", 10013);
+			udpClient = new DatagramSocket(0);
+			//udpClient.setSoTimeout(1000);
+			host = InetAddress.getByName("localhost");
 			
-			DatagramSocket udpClient = new DatagramSocket(0);
-			udpClient.setSoTimeout(1000);
+			is = new DataInputStream(client.getInputStream());
+			os = new DataOutputStream(client.getOutputStream());
+			isr = new InputStreamReader(is);
+			br = new BufferedReader(isr);
+	
 			
-			InetAddress host = InetAddress.getByName("localhost");
-			DatagramPacket request = new DatagramPacket(new byte[1024], 1024, host, 10014);
-			
-			byte[] data = new byte[1024];
-			DatagramPacket response = new DatagramPacket(data, data.length);
-			
-			udpClient.send(request);
-			udpClient.receive(response);
-			
-			String testString = new String(response.getData(), 0, response.getLength(), "US-ASCII");
-			
-			System.out.println(testString);
-			
-			
-			
-			DataInputStream is = new DataInputStream(client.getInputStream());
-			DataOutputStream os = new DataOutputStream(client.getOutputStream());
-			
-			InputStreamReader isr = new InputStreamReader(is);
-			BufferedReader br = new BufferedReader(isr);
-			System.out.println(br.readLine());
-			
-			client.close();
-			udpClient.close();
+			while (true) {
+				DatagramPacket request = new DatagramPacket(new byte[1024], 1024, host, 10028);
+				byte[] data = new byte[1024];
+				DatagramPacket response = new DatagramPacket(data, data.length);
+				
+				udpClient.send(request);
+				udpClient.receive(response);
+				
+				String testString = new String(response.getData(), 0,
+						response.getLength(), "US-ASCII");
+				System.out.println(testString);
+				
+				System.out.println(br.readLine());
+				
+			}
 			
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				client.close();
+				udpClient.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
 		}
 	}
-
 }
--- AudioStreaming/src/server/ServerTest.java
+++ AudioStreaming/src/server/ServerTest.java
@@ -1,52 +1,67 @@
 package server;
 
-import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import javax.sound.*;
-
 public class ServerTest {
 
-		public static void main(String[] args) {
-			try {
-				ServerSocket server = new ServerSocket(10013);
-				Socket client = server.accept();
-				
-				DatagramSocket udpServer = new DatagramSocket(10014);
-				DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
+	ServerSocket server;
+	Socket client;
+	
+	DatagramSocket udpServer;
+	DataOutputStream os;
+	
+	public static void main(String[] args) {
+		
+		ServerTest st = new ServerTest();
+		
+		st.run();
+	}
+		
+	private void run() {
+		try {
+			server = new ServerSocket(10027);
+			client = server.accept();
+			udpServer = new DatagramSocket(10028);
+			DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
+			
+			while (true) {
 				udpServer.receive(request);
-				
 				String testString = "Hello I am a string";
 				byte[] data = testString.getBytes("US-ASCII");
 				InetAddress host = request.getAddress();
 				int port = request.getPort();
 				DatagramPacket response = new DatagramPacket(data, data.length, host, port);
-				
 				udpServer.send(response);
+				Thread.sleep(1000);
 				
-				DataInputStream is = new DataInputStream(client.getInputStream());
-				DataOutputStream os = new DataOutputStream(client.getOutputStream());
-				
+				DataInputStream is = new DataInputStream(
+						client.getInputStream());
+				os = new DataOutputStream(client.getOutputStream());
 				os.writeUTF("Hello clients of the ServerTest");
-				
+			}
+			
+			
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			try {
 				client.close();
 				
 				os.close();
 				server.close();
 				udpServer.close();
-				
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 			
 			
 		}
+	}
 }
