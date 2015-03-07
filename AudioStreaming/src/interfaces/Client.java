8c9ca52530c13cee38f4ee9e39727930521547a0
igalna <igalna@gmail.com>
2015-03-07 13:17:57 +0000

    updated Client Interface with get socket

--- AudioStreaming/src/interfaces/Client.java
+++ AudioStreaming/src/interfaces/Client.java
@@ -1,10 +1,14 @@
 package interfaces;
 
+import java.net.Socket;
+
 public interface Client extends Runnable{
 
 	
 	public int getID();
 	
 	public String getRole();
+	
+	public Socket getSocket();
 	
 }
