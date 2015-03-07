48fc112f6f43855a8bcf6139b813f94efa68c2db
igalna <igalna@gmail.com>
2015-03-07 12:30:40 +0000

    Client Interface for new version of Client which has two methods for returning the ID number and the role of the client

--- AudioStreaming/src/interfaces/Client.java
+++ AudioStreaming/src/interfaces/Client.java
@@ -2,13 +2,9 @@ package interfaces;
 
 public interface Client extends Runnable{
 
-	public void connect();
 	
 	public int getID();
 	
-	public boolean isFirst();
-	
-	public void getUDPConnection();
-	
+	public String getRole();
 	
 }
