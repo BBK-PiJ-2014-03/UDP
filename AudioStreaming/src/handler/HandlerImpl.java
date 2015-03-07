7c883636234aa96fb3ca5602a6b0fdbd3e501e2b
igalna <igalna@gmail.com>
2015-03-07 13:21:38 +0000

    HandlerImpl takes the list of the servers clients and decides which is the sender and which are receivers

--- AudioStreaming/src/handler/HandlerImpl.java
+++ AudioStreaming/src/handler/HandlerImpl.java
@@ -4,7 +4,11 @@ import interfaces.Client;
 import interfaces.Handler;
 import interfaces.Server;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import client.ClientImpl;
 
 /**
  * 
@@ -32,12 +36,47 @@ public class HandlerImpl implements Handler {
 	public void run() {
 		// loop runs
 		while (true) {
-				// gets the current clients from the Server
-				ArrayList<Client> clientList = server.getClientList();
-				if (clientList[0] = null) {
-					
+			// gets the current clients from the Server
+			// each time the loop runs the newest list of clients is gathered
+			ArrayList<Client> clientList = server.getClientList();
+			// if there are no clients for the server to handle
+			// the handler thread goes to sleep
+			if (clientList.isEmpty()) {
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
-				
+			}
+			// if the server has clients
+			// the handler loops through the clients to discover if there is a SENDER client
+			else {
+				boolean hasSender = false;
+				for (Client c : clientList) {
+					if (c.getRole() == ClientImpl.SENDER) {
+						hasSender = true;
+					}
+				}
+				// if there isn't a sender the handler takes the next client
+				// in the list and sets it to be SENDER
+				if (hasSender == false) {
+					for (Client c : clientList) {
+						if (c != null) {
+							try {
+								DataOutputStream clientOutput = new DataOutputStream(c.getSocket().getOutputStream());
+								clientOutput.writeBytes(ClientImpl.SENDER);
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
+					}
+				}
+			}
+			
+			// after updating the server's clients
+			// thread then relays audio data from the sender to the receivers
+			
+			
 		}
 
 	}
