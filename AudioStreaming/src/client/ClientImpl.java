1a0c25fb39dc492ca6176b5ec2c816999943c194
igalna <igalna@gmail.com>
2015-03-07 13:04:20 +0000

    Commmit with added documentation

--- AudioStreaming/src/client/ClientImpl.java
+++ AudioStreaming/src/client/ClientImpl.java
@@ -61,14 +61,17 @@ public class ClientImpl implements Client {
 	@Override
 	public void run() {
 		try {
+			// establishes the client with the server
 			client = new Socket(GROUP, PORT);
-			DataInputStream is = new DataInputStream(client.getInputStream());
-			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			DataInputStream is = new DataInputStream(
+					client.getInputStream());
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(is));
 			// asks the server for a unique ID.
 			this.ID = Integer.parseInt(br.readLine());
 			// asks the server whether it is the first to connect.
 			this.role = br.readLine();
-			
+
 			// if statement checks if the user is the Sender
 			if (role.equals(SENDER)) {
 				DatagramSocket UDPSocket = new DatagramSocket();
@@ -81,7 +84,8 @@ public class ClientImpl implements Client {
 				fis.read(fileArray);
 				// sends the fileArray to the server
 				while (UDPSocket.isConnected()) {
-					DatagramPacket sendPacket = new DatagramPacket(fileArray, fileArray.length, address, PORT);
+					DatagramPacket sendPacket = new DatagramPacket(
+							fileArray, fileArray.length, address, PORT);
 					UDPSocket.send(sendPacket);
 				}
 				fis.close();
@@ -94,7 +98,8 @@ public class ClientImpl implements Client {
 				InetAddress address = client.getInetAddress();
 				UDPSocket.connect(address, PORT);
 				while (UDPSocket.isConnected()) {
-					DatagramPacket response = new DatagramPacket(new byte[BYTELENGTH], BYTELENGTH);
+					DatagramPacket response = new DatagramPacket(
+							new byte[BYTELENGTH], BYTELENGTH);
 					UDPSocket.receive(response);
 					// bytes received from the server are written to a File
 					FileOutputStream fos = new FileOutputStream(receiveFile);
@@ -102,8 +107,7 @@ public class ClientImpl implements Client {
 				}
 				UDPSocket.close();
 			}
-				
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
