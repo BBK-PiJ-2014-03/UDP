66f9081352c208fa5d776ea3c76e55d12a34cc00
igalna <igalna@gmail.com>
2015-03-14 18:00:40 +0000

    javadoc for Server

--- AudioStreaming/src/interfaces/Server.java
+++ AudioStreaming/src/interfaces/Server.java
@@ -3,10 +3,33 @@ package interfaces;
 import java.net.DatagramSocket;
 import java.util.ArrayList;
 
-
+/**
+ * 
+ * interface representing a server
+ * 
+ * @author IAN
+ *
+ */
 public interface Server {
 	
+	/**
+	 * gets a unique id number to send to clients
+	 * 
+	 * @return the servers client id
+	 */
 	public int getUniqueID();
+	/**
+	 * gets a list of the servers clients
+	 * 
+	 * @return the ArrayList of the clients
+	 */
 	public ArrayList<Client> getClientList();
+	
+	/**
+	 * 
+	 * gets the datagram socket that this server uses to relay data
+	 * 
+	 * @return the servers datagram socket
+	 */
 	public DatagramSocket getSocket();
 }
